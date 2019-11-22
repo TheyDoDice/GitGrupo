e1952c6a1c25a16f9908a27d667255871d451bae
Adrià Granero <50867803+adrigr4@users.noreply.github.com>
2019-11-22 18:29:56 +0100

    ..

--- MantenimientoBBDD/FormsMantemimiento/MantenimientoUserCategories.Designer.cs
+++ MantenimientoBBDD/FormsMantemimiento/MantenimientoUserCategories.Designer.cs
@@ -34,6 +34,10 @@
             this.swt_codeSpecie = new CC_Library.SWTextbox();
             this.swTextbox1 = new CC_Library.SWTextbox();
             this.swTextbox2 = new CC_Library.SWTextbox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_actualizarDataset = new System.Windows.Forms.Button();
+            this.btn_insertar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // lb_codeSpecie
@@ -65,9 +69,9 @@
             this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(53, 172);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(111, 24);
+            this.label2.Size = new System.Drawing.Size(117, 24);
             this.label2.TabIndex = 73;
-            this.label2.Text = "Access Level";
+            this.label2.Text = "Access Level:";
             // 
             // swt_codeSpecie
             // 
@@ -79,7 +83,7 @@
             this.swt_codeSpecie.Location = new System.Drawing.Point(263, 57);
             this.swt_codeSpecie.mensaje = null;
             this.swt_codeSpecie.Name = "swt_codeSpecie";
-            this.swt_codeSpecie.nomCamp = "CodeSpecie";
+            this.swt_codeSpecie.nomCamp = "CodeCategory";
             this.swt_codeSpecie.nomComboBox = null;
             this.swt_codeSpecie.requerit = false;
             this.swt_codeSpecie.Size = new System.Drawing.Size(194, 32);
@@ -96,7 +100,7 @@
             this.swTextbox1.Location = new System.Drawing.Point(263, 112);
             this.swTextbox1.mensaje = null;
             this.swTextbox1.Name = "swTextbox1";
-            this.swTextbox1.nomCamp = "CodeSpecie";
+            this.swTextbox1.nomCamp = "DescCategory";
             this.swTextbox1.nomComboBox = null;
             this.swTextbox1.requerit = false;
             this.swTextbox1.Size = new System.Drawing.Size(194, 32);
@@ -113,19 +117,60 @@
             this.swTextbox2.Location = new System.Drawing.Point(263, 169);
             this.swTextbox2.mensaje = null;
             this.swTextbox2.Name = "swTextbox2";
-            this.swTextbox2.nomCamp = "CodeSpecie";
+            this.swTextbox2.nomCamp = "AccessLevel";
             this.swTextbox2.nomComboBox = null;
             this.swTextbox2.requerit = false;
             this.swTextbox2.Size = new System.Drawing.Size(194, 32);
             this.swTextbox2.TabIndex = 76;
             this.swTextbox2.TipusControl = CC_Library.SWTextbox.tipControlRef.SWComboBox;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 318);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(840, 294);
+            this.dataGridView1.TabIndex = 77;
+            // 
+            // btn_actualizarDataset
+            // 
+            this.btn_actualizarDataset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(57)))), ((int)(((byte)(57)))));
+            this.btn_actualizarDataset.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_actualizarDataset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_actualizarDataset.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_actualizarDataset.ForeColor = System.Drawing.Color.White;
+            this.btn_actualizarDataset.Location = new System.Drawing.Point(12, 271);
+            this.btn_actualizarDataset.Name = "btn_actualizarDataset";
+            this.btn_actualizarDataset.Size = new System.Drawing.Size(117, 29);
+            this.btn_actualizarDataset.TabIndex = 79;
+            this.btn_actualizarDataset.Text = "Actualizar";
+            this.btn_actualizarDataset.UseVisualStyleBackColor = false;
+            this.btn_actualizarDataset.Click += new System.EventHandler(this.btn_actualizarDataset_Click);
+            // 
+            // btn_insertar
+            // 
+            this.btn_insertar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(57)))), ((int)(((byte)(57)))));
+            this.btn_insertar.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_insertar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_insertar.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_insertar.ForeColor = System.Drawing.Color.White;
+            this.btn_insertar.Location = new System.Drawing.Point(146, 271);
+            this.btn_insertar.Name = "btn_insertar";
+            this.btn_insertar.Size = new System.Drawing.Size(117, 29);
+            this.btn_insertar.TabIndex = 80;
+            this.btn_insertar.Text = "Insertar";
+            this.btn_insertar.UseVisualStyleBackColor = false;
+            this.btn_insertar.Click += new System.EventHandler(this.btn_insertar_Click);
+            // 
             // MantenimientoUserCategories
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(103)))), ((int)(((byte)(103)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(864, 624);
+            this.Controls.Add(this.btn_insertar);
+            this.Controls.Add(this.btn_actualizarDataset);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.swTextbox2);
             this.Controls.Add(this.swTextbox1);
             this.Controls.Add(this.swt_codeSpecie);
@@ -134,6 +179,8 @@
             this.Controls.Add(this.lb_codeSpecie);
             this.Name = "MantenimientoUserCategories";
             this.Text = "MantenimientoUserCategories";
+            this.Load += new System.EventHandler(this.MantenimientoUserCategories_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,5 +194,8 @@
         private CC_Library.SWTextbox swt_codeSpecie;
         private CC_Library.SWTextbox swTextbox1;
         private CC_Library.SWTextbox swTextbox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_actualizarDataset;
+        private System.Windows.Forms.Button btn_insertar;
     }
 }
--- MantenimientoBBDD/FormsMantemimiento/MantenimientoUserCategories.cs
+++ MantenimientoBBDD/FormsMantemimiento/MantenimientoUserCategories.cs
@@ -8,17 +8,54 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BBDD;
+using System.Reflection;
+using CC_Library;
 
 namespace FormsMantemimiento
 {
     public partial class MantenimientoUserCategories : Form
     {
+        securecoreEntities db = new securecoreEntities();
+
         public MantenimientoUserCategories()
         {
             InitializeComponent();
+
+        }       
+
+
+        private void MantenimientoUserCategories_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.UserCategories.ToList();
+            
         }
 
-        securecoreEntities scEntities = new securecoreEntities();
+        private void btn_insertar_Click(object sender, EventArgs e)
+        {
+            UserCategories Category = new UserCategories();
+            foreach (var control in this.Controls)
+            {
+                if (control is SWTextbox swt)
+                {
+                    try
+                    {
+                        int num = int.Parse(swt.Text);
+                        Category.GetType().GetProperty(swt.nomCamp).SetValue(Category, num);
+                    }
+                    catch (Exception)
+                    {
+                        Category.GetType().GetProperty(swt.nomCamp).SetValue(Category, swt.Text);
+                    }
+                }
+            }
+            db.UserCategories.Add(Category);
+            db.SaveChanges();
+            dataGridView1.DataSource = db.UserCategories.ToList();
+        }
 
+        private void btn_actualizarDataset_Click(object sender, EventArgs e)
+        {
+            UserCategories Category = db.UserCategories.ToList().Find(x => x.idUserCategory == 1);
+        }
     }
 }
