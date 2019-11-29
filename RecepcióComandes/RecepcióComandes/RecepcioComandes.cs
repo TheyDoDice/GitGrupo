2ab4410a3d1c1d40a6b9ee3643aae92bfff0c393
jordi2505 <55592657+jordi2505@users.noreply.github.com>
2019-11-29 20:34:53 +0100

    Programa FTP
    Añadidas las funciones para crear carpetas y subir archivos, la visión de arbol ya funciona y también ya puede subir archivos.

--- RecepcióComandes/RecepcióComandes/RecepcioComandes.cs
+++ RecepcióComandes/RecepcióComandes/RecepcioComandes.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace RecepcióComandes
 {
@@ -42,19 +43,25 @@ namespace RecepcióComandes
             barraSuperior.MouseDown += (se, ev) => ptb_maximize.Image = this.WindowState != FormWindowState.Maximized ? TakeImg("dark", "maximize_2") : TakeImg("dark", "maximize_1");
 
             #endregion
-        
+
             #region FOCUS TEXTBOX
             txtb_Servidor.Leave += (se, ev) => txtb_Servidor.BackColor = System.Drawing.ColorTranslator.FromHtml("#393939");
             txtb_Servidor.Enter += (se, ev) => txtb_Servidor.BackColor = System.Drawing.ColorTranslator.FromHtml("#535353");
 
-            txtb_user.Leave += (se, ev) => txtb_user.BackColor = System.Drawing.ColorTranslator.FromHtml("#393939");
-            txtb_user.Enter += (se, ev) => txtb_user.BackColor = System.Drawing.ColorTranslator.FromHtml("#535353");
+            txtb_Usuario.Leave += (se, ev) => txtb_Usuario.BackColor = System.Drawing.ColorTranslator.FromHtml("#393939");
+            txtb_Usuario.Enter += (se, ev) => txtb_Usuario.BackColor = System.Drawing.ColorTranslator.FromHtml("#535353");
 
-            txtb_contraseña.Leave += (se, ev) => txtb_contraseña.BackColor = System.Drawing.ColorTranslator.FromHtml("#393939");
-            txtb_contraseña.Enter += (se, ev) => txtb_contraseña.BackColor = System.Drawing.ColorTranslator.FromHtml("#535353");
+            txtb_Contraseña.Leave += (se, ev) => txtb_Contraseña.BackColor = System.Drawing.ColorTranslator.FromHtml("#393939");
+            txtb_Contraseña.Enter += (se, ev) => txtb_Contraseña.BackColor = System.Drawing.ColorTranslator.FromHtml("#535353");
 
-            txtb_puerto.Leave += (se, ev) => txtb_puerto.BackColor = System.Drawing.ColorTranslator.FromHtml("#393939");
-            txtb_puerto.Enter += (se, ev) => txtb_puerto.BackColor = System.Drawing.ColorTranslator.FromHtml("#535353");
+            txtb_Puerto.Leave += (se, ev) => txtb_Puerto.BackColor = System.Drawing.ColorTranslator.FromHtml("#393939");
+            txtb_Puerto.Enter += (se, ev) => txtb_Puerto.BackColor = System.Drawing.ColorTranslator.FromHtml("#535353");
+
+            txtb_RutaCarpetaDescargas.Leave += (se, ev) => txtb_RutaCarpetaDescargas.BackColor = System.Drawing.ColorTranslator.FromHtml("#393939");
+            txtb_RutaCarpetaDescargas.Enter += (se, ev) => txtb_RutaCarpetaDescargas.BackColor = System.Drawing.ColorTranslator.FromHtml("#535353");
+
+            cbx_Impresora.Leave += (se, ev) => cbx_Impresora.BackColor = System.Drawing.ColorTranslator.FromHtml("#393939");
+            cbx_Impresora.Enter += (se, ev) => cbx_Impresora.BackColor = System.Drawing.ColorTranslator.FromHtml("#535353");
 
             #endregion
         }
@@ -78,80 +85,290 @@ namespace RecepcióComandes
 
         #endregion
 
+        #region FUNCIONES PARA EL VISOR DE ARCHIVOS
+
+        private TreeNode CreateDirectoryNode(string path, string name)
+        {
+            var directoryNode = new TreeNode(name);
+            var directoryListing = GetDirectoryListing(path);
+
+            var directories = directoryListing.Where(d => d.IsDirectory);
+            var files = directoryListing.Where(d => !d.IsDirectory);
+
+            foreach (var dir in directories)
+            {
+                directoryNode.Nodes.Add(CreateDirectoryNode(dir.FullPath, dir.Name));
+            }
+            foreach (var file in files)
+            {
+                directoryNode.Nodes.Add(new TreeNode(file.Name).ToString(), file.Name.ToString(), 1, 1);
+            }
+            return directoryNode;
+        }
+
+        public class FTPListDetail
+        {
+            public bool IsDirectory
+            {
+                get
+                {
+                    return !string.IsNullOrWhiteSpace(Dir) && Dir.ToLower().Equals("d");
+                }
+            }
+            internal string Dir { get; set; }
+            public string Permission { get; set; }
+            public string Filecode { get; set; }
+            public string Owner { get; set; }
+            public string Group { get; set; }
+            public string Name { get; set; }
+            public string FullPath { get; set; }
+        }
+
+        public IEnumerable<FTPListDetail> GetDirectoryListing(string rootUri)
+        {
+            var CurrentRemoteDirectory = rootUri;
+            var result = new StringBuilder();
+            var request = GetWebRequest(WebRequestMethods.Ftp.ListDirectoryDetails, CurrentRemoteDirectory);
+            using (var response = request.GetResponse())
+            {
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        result.Append(line);
+                        result.Append("\n");
+                        line = reader.ReadLine();
+                    }
+                    if (string.IsNullOrEmpty(result.ToString()))
+                    {
+                        return new List<FTPListDetail>();
+                    }
+                    result.Remove(result.ToString().LastIndexOf("\n"), 1);
+                    var results = result.ToString().Split('\n');
+                    string regex =
+                        @"^" +               //# Start of line
+                        @"(?<dir>[\-ld])" +          //# File size          
+                        @"(?<permission>[\-rwx]{9})" +            //# Whitespace          \n
+                        @"\s+" +            //# Whitespace          \n
+                        @"(?<filecode>\d+)" +
+                        @"\s+" +            //# Whitespace          \n
+                        @"(?<owner>\w+)" +
+                        @"\s+" +            //# Whitespace          \n
+                        @"(?<group>\w+)" +
+                        @"\s+" +            //# Whitespace          \n
+                        @"(?<size>\d+)" +
+                        @"\s+" +            //# Whitespace          \n
+                        @"(?<month>\w{3})" +          //# Month (3 letters)   \n
+                        @"\s+" +            //# Whitespace          \n
+                        @"(?<day>\d{1,2})" +        //# Day (1 or 2 digits) \n
+                        @"\s+" +            //# Whitespace          \n
+                        @"(?<timeyear>[\d:]{4,5})" +     //# Time or year        \n
+                        @"\s+" +            //# Whitespace          \n
+                        @"(?<filename>(.*))" +            //# Filename            \n
+                        @"$";                //# End of line
+
+                    var myresult = new List<FTPListDetail>();
+                    foreach (var parsed in results)
+                    {
+                        var split = new Regex(regex)
+                            .Match(parsed);
+                        var dir = split.Groups["dir"].ToString();
+                        var permission = split.Groups["permission"].ToString();
+                        var filecode = split.Groups["filecode"].ToString();
+                        var owner = split.Groups["owner"].ToString();
+                        var group = split.Groups["group"].ToString();
+                        var filename = split.Groups["filename"].ToString();
+                        myresult.Add(new FTPListDetail()
+                        {
+                            Dir = dir,
+                            Filecode = filecode,
+                            Group = group,
+                            FullPath = CurrentRemoteDirectory + "/" + filename,
+                            Name = filename,
+                            Owner = owner,
+                            Permission = permission,
+                        });
+                    };
+                    return myresult;
+                }
+            }
+        }
+
+        private FtpWebRequest GetWebRequest(string method, string uri)
+        {
+            Uri serverUri = new Uri(uri);
+            if (serverUri.Scheme != Uri.UriSchemeFtp)
+            {
+                return null;
+            }
+            var reqFTP = (FtpWebRequest)FtpWebRequest.Create(serverUri);
+            reqFTP.Method = method;
+            reqFTP.UseBinary = true;
+            reqFTP.Credentials = new NetworkCredential(txtb_Usuario.Text.Trim(), txtb_Contraseña.Text.Trim());
+            reqFTP.Proxy = null;
+            reqFTP.KeepAlive = false;
+            reqFTP.UsePassive = false;
+            return reqFTP;
+        }
+
+        #endregion
+
+        #region FUNCIONES SUBIR ARCHIVOS Y CREAR CARPETAS
+
+        //Función para crear una carpeta en el servidor FTP
+        private void CrearCarpeta(string ip_servidor, string nombre_carpeta, string usuario, string contraseña)
+        {
+            FtpWebRequest CrearCarpeta = (FtpWebRequest)WebRequest.Create(new Uri(string.Format(@"ftp://{0}/{1}/", ip_servidor, nombre_carpeta)));
+            CrearCarpeta.Credentials = new NetworkCredential(usuario, contraseña);
+            CrearCarpeta.Method = WebRequestMethods.Ftp.MakeDirectory;
+            FtpWebResponse ftpResponse = (FtpWebResponse)CrearCarpeta.GetResponse();
+        }
+
+        //Funcion para subir un archivo a una carpeta FTP
+        private void SubirArchivoCarpeta(string ip_servidor, string nombre_carpeta, string rutaAbsolutaArchivo, string nombreArchivo, string usuario, string contraseña)
+        {
+            FtpWebRequest SubirArchivo = (FtpWebRequest)WebRequest.Create("ftp://" + ip_servidor + "/" + nombre_carpeta + "/" + nombreArchivo);
+            SubirArchivo.Credentials = new NetworkCredential(usuario, contraseña);
+            SubirArchivo.Method = WebRequestMethods.Ftp.UploadFile;
+            FtpWebResponse ftpResponse = (FtpWebResponse)SubirArchivo.GetResponse();
+
+            using (Stream fileStream = File.OpenRead(rutaAbsolutaArchivo))
+            using (Stream ftpStream = SubirArchivo.GetRequestStream())
+            {
+                fileStream.CopyTo(ftpStream);
+            }
+        }
+#endregion
+
         private Image TakeImg(string mode, string name)
         {
             return Image.FromFile(Application.StartupPath + "\\Img\\" + mode + "_" + name + ".png");
         }
 
-        //Ruta on es generará el XML amb les credencials.
-        private static string RutaArxiuXML = Application.StartupPath + "\\credenciales.xml";
-        private static string downloadFolder = Application.StartupPath + "\\Descargas";
+        //Ruta donde se generará el XML con las credenciales
+        private static string RutaArchivoXML = Application.StartupPath + "\\credenciales.xml";
 
-        private void btn_conectar_Click(object sender, EventArgs e)
-        {
-            //Declarar variables
-            string serv, user, pass, port;
+        //Ruta donde se guardarán los archivos descargados
+        private static string CarpetaDescargas = Application.StartupPath + "\\Descargas";
 
-            //Llegir dades Textbox
-            pass = txtb_contraseña.Text;
-            serv = txtb_Servidor.Text;
-            user = txtb_user.Text;
+        //Objeto para leer los XML
+        private XDocument Credenciales = XDocument.Load(RutaArchivoXML);
 
-            //Si el textbox puerto está en blanc ficar el port predeterminat
-            if (string.IsNullOrEmpty(txtb_puerto.Text.Trim()))
-            {
-                port = "21";
-            }
-            else
-            {
-                port = txtb_puerto.Text;
-            }
+        //Explorador de Carpetas
+        private FolderBrowserDialog SelectorCarpetas = new FolderBrowserDialog();
 
-            //Encriptar valors i escriure Dades Arxiu .txt
-            XmlWriter writer = XmlWriter.Create(RutaArxiuXML);
-            writer.WriteStartElement("Credencials");
-            writer.WriteElementString("IP",serv.Trim());
-            writer.WriteElementString("Port", port.Trim());
-            writer.WriteElementString("User", user.Trim());
-            writer.WriteElementString("Password", pass.Trim());
-            writer.WriteElementString("CarpetaBaixada", downloadFolder.Trim());
-            writer.WriteEndElement();
-            writer.Flush();
+        //Explorador de Archivos
+        private OpenFileDialog ExploradorArchivos = new OpenFileDialog();
+        private Uri CadenaConnexionFTP = new Uri("ftp://8.8.8.8/");
 
-            /*
-            DADES FTP:
-            -IP: 172.17.6.0
-            -User: g02
-            -Pass: 12345aA
-            -Port: 22
-             * */
-
-        }
-
+        //Codigo que se ejecuta al inicio
         private void RecepcióDeComandes_Load(object sender, EventArgs e)
         {
+            //Lista de imagenes para el visor de archivos
             ImageList myImageList = new ImageList();
             myImageList.Images.Add(Image.FromFile(Application.StartupPath + "\\Img\\carpeta.png"));
             myImageList.Images.Add(Image.FromFile(Application.StartupPath + "\\Img\\archivo.png"));
             VisorArchivos.ImageList = myImageList;
 
-            txtb_ruta.Text = downloadFolder;
-        }
-
-        private void btn_descargar_Click(object sender, EventArgs e)
-        {
-        }
-
-        //Ruta on es guardarán les descarregues
-        private void txtb_ruta_MouseClick(object sender, MouseEventArgs e)
-        {
-            var fbd = new FolderBrowserDialog();
-
-            DialogResult result = fbd.ShowDialog();
-
-            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            //Leer datos XML y ponerlos en los Textbox
+            foreach (XElement node in Credenciales.Descendants("Credencials"))
             {
-                txtb_ruta.Text = fbd.SelectedPath;
+                txtb_Servidor.Text = node.Element("IP").Value;
+                txtb_Puerto.Text = node.Element("Port").Value;
+                txtb_Usuario.Text = node.Element("User").Value;
+                txtb_Contraseña.Text = node.Element("Password").Value;
+                txtb_RutaCarpetaDescargas.Text = node.Element("CarpetaBaixada").Value;
+            }
+
+            //Objeto conexión 
+            CadenaConnexionFTP = new Uri("ftp://" + txtb_Servidor.Text + "/");
+    }
+
+        //Botón para cambiar la ruta donde se guardan los archivos descargados
+        private void btn_CambiarCarpetaDescargas_Click(object sender, EventArgs e)
+        {
+            //Abrir explorador de carpetas 
+            DialogResult result = SelectorCarpetas.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(SelectorCarpetas.SelectedPath))
+            {
+                //Cambiar y guardar la ruta
+                txtb_RutaCarpetaDescargas.Text = SelectorCarpetas.SelectedPath;
+                foreach (XElement node in Credenciales.Descendants("Credencials"))
+                {
+                    node.SetElementValue("CarpetaBaixada", SelectorCarpetas.SelectedPath);
+                }
+
+                Credenciales.Save(RutaArchivoXML);
+            }
+        }
+
+        //Botón guardar
+        private void btn_Guardar_Click(object sender, EventArgs e)
+        {
+            //Declarar variables
+            string serv, user, pass, port;
+
+            //Leer datos de los textbox
+            pass = txtb_Contraseña.Text.Trim();
+            serv = txtb_Servidor.Text.Trim();
+            user = txtb_Usuario.Text.Trim();
+
+            //Si el textbox puerto está en blanc ficar el port predeterminat
+            if (string.IsNullOrEmpty(txtb_Puerto.Text.Trim()))
+            {
+                port = "21"; //Puerto predeterminado para FTP
+            }
+            else
+            {
+                port = txtb_Puerto.Text;
+            }
+
+            /*DADES FTP:  -IP: 172.17.6.0     -User: g02      -Pass: 12345aA      -Port: 22*/
+
+            //Escribir credenciales en el XML
+            foreach (XElement node in Credenciales.Descendants("Credencials"))
+            {
+                node.SetElementValue("IP", serv);
+                node.SetElementValue("Port", port);
+                node.SetElementValue("User", user);
+                node.SetElementValue("Password", pass);
+                node.SetElementValue("CarpetaBaixada", CarpetaDescargas);
+            }
+            Credenciales.Save(RutaArchivoXML);
+        }
+
+        //Botón para conectarse y comprobar si hay cambios en los archivos
+        private void btn_Check_Click(object sender, EventArgs e)
+        {
+            VisorArchivos.BeginUpdate();
+            VisorArchivos.Nodes.Clear();
+            VisorArchivos.Nodes.Add(CreateDirectoryNode(CadenaConnexionFTP.ToString(), "Directori Principal"));
+            VisorArchivos.EndUpdate();
+        }
+
+        //Explorador archivos para subir al servidor      
+        private void btn_AbrirArchivos_Click(object sender, EventArgs e)
+        {
+            string RutaArchivo;
+
+            //Abrir Explorador de Archivos
+            ExploradorArchivos.InitialDirectory = "C:\\";
+            ExploradorArchivos.Filter = "Archivos EDI (*.EDI)|*.EDI|All files (*.*)|*.*";
+            ExploradorArchivos.FilterIndex = 0;
+            ExploradorArchivos.RestoreDirectory = true;
+
+            if (ExploradorArchivos.ShowDialog() == DialogResult.OK)
+            {
+                //Guardar ruta del archivo seleccionado
+                RutaArchivo = ExploradorArchivos.FileName;
+
+                //CrearCarpeta
+                CrearCarpeta(txtb_Servidor.Text.Trim(), "testFolder", txtb_Usuario.Text.Trim(), txtb_Contraseña.Text.Trim());
+
+                //SubirArchivo a esa carpeta
+                SubirArchivoCarpeta(txtb_Servidor.Text.Trim(), "testFolder", RutaArchivo, ExploradorArchivos.SafeFileName, txtb_Usuario.Text.Trim(), txtb_Contraseña.Text.Trim());
             }
         }
     }
