6e3618586c2fa770d09767baf206e03b6aa07e9b
TheyDoDice <alberto.munoz@sarria.salesians.cat>
2020-01-15 16:35:05 +0100

    Crystal Reports acabado

--- MantenimientoBBDD/FormsMantemimiento/MantenimientoOrders.cs
+++ MantenimientoBBDD/FormsMantemimiento/MantenimientoOrders.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Text;
 using System.Windows.Forms;
 using BBDD;
+using CrystalDecisions.CrystalReports.Engine;
 
 namespace FormsMantemimiento
 {
@@ -13,6 +15,8 @@ namespace FormsMantemimiento
     {
         public string id = "nada";
         SQL database = new SQL();
+        List<string> impresoras = new List<string>();
+
 
         public MantenimientoOrders()
         {
@@ -21,6 +25,12 @@ namespace FormsMantemimiento
 
         private void MantenimientoOrders_Load(object sender, EventArgs e)
         {
+            foreach (var item in System.Drawing.Printing.PrinterSettings.InstalledPrinters)
+            {
+                impresoras.Add(item.ToString());
+            }
+            comboBox1.DataSource = impresoras;
+
             label1.MouseEnter += (s, ev) => label1.ForeColor = Color.DarkGray;
             label1.MouseLeave += (s, ev) => label1.ForeColor = Color.White;
 
@@ -45,6 +55,39 @@ namespace FormsMantemimiento
         {
             panel3.Size = panel3.Size.Width == 2 ? new System.Drawing.Size(772, 520) : new System.Drawing.Size(2, 520);
             panel2.Visible = panel2.Visible ? false : true;
+        }
+
+        private void btn_imprimir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ReportDocument rpd = new ReportDocument();
+                rpd.Load(Application.StartupPath + @"\CrystalReport\CROrders.rpt");
+                rpd.SetParameterValue("id", id);
+                rpd.SetDatabaseLogon("TheyDoDice", "123456aA");
+
+                PrinterSettings print = new PrinterSettings();
+                print.PrinterName = comboBox1.Text;
+
+                rpd.PrintToPrinter(print, new PageSettings(), false);
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("selecciona una fila completa");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(comboBox1.Text))
+            {
+                btn_imprimir.Enabled = true;
+            }
+            else
+            {
+                btn_imprimir.Enabled = false;
+            }
         }
     }
 }
