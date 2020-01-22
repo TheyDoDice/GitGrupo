613e18510b09d0a2101fd32eda6d74b6630a2444
TheyDoDice <alberto.munoz@sarria.salesians.cat>
2020-01-22 16:16:18 +0100

    boton accept tareas

--- Tareas/Tareas/InicioSesionTareas.Designer.cs
+++ Tareas/Tareas/InicioSesionTareas.Designer.cs
@@ -148,6 +148,7 @@
             // 
             // InicioSesionTareas
             // 
+            this.AcceptButton = this.btn_login;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(442, 249);
