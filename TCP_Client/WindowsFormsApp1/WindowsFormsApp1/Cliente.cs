9fe44e7d955124a9673853c607ea83e311485638
Adrià Granero <50867803+adrigr4@users.noreply.github.com>
2020-02-20 16:45:02 +0100

    Mejoras Cliente

--- TCP_Client/WindowsFormsApp1/WindowsFormsApp1/Cliente.cs
+++ TCP_Client/WindowsFormsApp1/WindowsFormsApp1/Cliente.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.Sockets;
-using System.Net;
 namespace WindowsFormsApp1
 {
     public partial class Cliente : Form
@@ -20,22 +13,28 @@ namespace WindowsFormsApp1
 
         private void bttn_enviarDades_Click(object sender, EventArgs e)
         {
-           
-            TcpClient client = new TcpClient(tbx_ip.Text.Trim(), Int32.Parse(tbx_port.Text.Trim()));
-            Byte[] dades = Encoding.ASCII.GetBytes(tbx_Message.Text);
+            try
+            {
+                TcpClient client = new TcpClient(tbx_ip.Text.Trim(), Int32.Parse(tbx_port.Text.Trim()));
+                Byte[] dades = Encoding.ASCII.GetBytes(tbx_Message.Text);
 
-            NetworkStream ns = client.GetStream();
+                NetworkStream ns = client.GetStream();
 
-            //Enviar dades al servidor
-             ns.Write(dades, 0, dades.Length);
+                //Enviar dades al servidor
+                ns.Write(dades, 0, dades.Length);
 
-            //Rebre Resposta servidor
-            byte[] dadaResposta = new Byte[client.ReceiveBufferSize];
-            String resposta = textBox2.Text;
-            Int32 bytes = ns.Read(dadaResposta, 0, dadaResposta.Length);
-            resposta = Encoding.ASCII.GetString(dadaResposta);
+                //Rebre Resposta servidor
+                byte[] dadaResposta = new Byte[client.ReceiveBufferSize];
+                String resposta = textBox2.Text;
+                Int32 bytes = ns.Read(dadaResposta, 0, dadaResposta.Length);
+                resposta = Encoding.ASCII.GetString(dadaResposta);
 
-            MessageBox.Show(resposta, "Resposta Servidor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show(resposta, "Resposta Servidor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
