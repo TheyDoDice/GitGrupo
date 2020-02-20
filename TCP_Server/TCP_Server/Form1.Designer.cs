9eac5248f98fe9739ce470791a099cc51ca02e69
TheyDoDice <alberto.munoz@sarria.salesians.cat>
2020-02-20 16:44:22 +0100

    Tcp server actualizado

--- TCP_Server/TCP_Server/Form1.Designer.cs
+++ TCP_Server/TCP_Server/Form1.Designer.cs
@@ -30,16 +30,11 @@
         {
             this.components = new System.ComponentModel.Container();
             this.grbx_datos = new System.Windows.Forms.GroupBox();
-            this.btn_desconectar = new System.Windows.Forms.Button();
-            this.btn_conectar = new System.Windows.Forms.Button();
             this.txtb_port = new System.Windows.Forms.TextBox();
             this.lbl_puerto = new System.Windows.Forms.Label();
-            this.txtb_ip = new System.Windows.Forms.TextBox();
-            this.lbl_ip = new System.Windows.Forms.Label();
             this.gb_datosRecibidos = new System.Windows.Forms.GroupBox();
-            this.btn_respuesta = new System.Windows.Forms.Button();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.txtb_datosRecibidos = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.grbx_datos.SuspendLayout();
             this.gb_datosRecibidos.SuspendLayout();
             this.SuspendLayout();
@@ -48,71 +43,31 @@
             // 
             this.grbx_datos.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.grbx_datos.Controls.Add(this.btn_respuesta);
-            this.grbx_datos.Controls.Add(this.btn_desconectar);
-            this.grbx_datos.Controls.Add(this.btn_conectar);
             this.grbx_datos.Controls.Add(this.txtb_port);
             this.grbx_datos.Controls.Add(this.lbl_puerto);
-            this.grbx_datos.Controls.Add(this.txtb_ip);
-            this.grbx_datos.Controls.Add(this.lbl_ip);
             this.grbx_datos.Location = new System.Drawing.Point(12, 12);
             this.grbx_datos.Name = "grbx_datos";
-            this.grbx_datos.Size = new System.Drawing.Size(515, 46);
+            this.grbx_datos.Size = new System.Drawing.Size(281, 46);
             this.grbx_datos.TabIndex = 0;
             this.grbx_datos.TabStop = false;
             this.grbx_datos.Text = "Datos";
             // 
-            // btn_desconectar
-            // 
-            this.btn_desconectar.Location = new System.Drawing.Point(345, 15);
-            this.btn_desconectar.Name = "btn_desconectar";
-            this.btn_desconectar.Size = new System.Drawing.Size(79, 23);
-            this.btn_desconectar.TabIndex = 5;
-            this.btn_desconectar.Text = "Desconectar";
-            this.btn_desconectar.UseVisualStyleBackColor = true;
-            this.btn_desconectar.Click += new System.EventHandler(this.btn_desconectar_Click);
-            // 
-            // btn_conectar
-            // 
-            this.btn_conectar.Location = new System.Drawing.Point(281, 15);
-            this.btn_conectar.Name = "btn_conectar";
-            this.btn_conectar.Size = new System.Drawing.Size(58, 23);
-            this.btn_conectar.TabIndex = 4;
-            this.btn_conectar.Text = "Conectar";
-            this.btn_conectar.UseVisualStyleBackColor = true;
-            this.btn_conectar.Click += new System.EventHandler(this.btn_conectar_Click);
-            // 
             // txtb_port
             // 
-            this.txtb_port.Location = new System.Drawing.Point(230, 17);
+            this.txtb_port.Location = new System.Drawing.Point(57, 15);
             this.txtb_port.Name = "txtb_port";
-            this.txtb_port.Size = new System.Drawing.Size(45, 20);
+            this.txtb_port.Size = new System.Drawing.Size(206, 20);
             this.txtb_port.TabIndex = 3;
+            this.txtb_port.Text = "8888";
             // 
             // lbl_puerto
             // 
             this.lbl_puerto.AutoSize = true;
-            this.lbl_puerto.Location = new System.Drawing.Point(183, 20);
+            this.lbl_puerto.Location = new System.Drawing.Point(10, 18);
             this.lbl_puerto.Name = "lbl_puerto";
             this.lbl_puerto.Size = new System.Drawing.Size(41, 13);
             this.lbl_puerto.TabIndex = 2;
             this.lbl_puerto.Text = "Puerto:";
-            // 
-            // txtb_ip
-            // 
-            this.txtb_ip.Location = new System.Drawing.Point(33, 17);
-            this.txtb_ip.Name = "txtb_ip";
-            this.txtb_ip.Size = new System.Drawing.Size(144, 20);
-            this.txtb_ip.TabIndex = 1;
-            // 
-            // lbl_ip
-            // 
-            this.lbl_ip.AutoSize = true;
-            this.lbl_ip.Location = new System.Drawing.Point(7, 20);
-            this.lbl_ip.Name = "lbl_ip";
-            this.lbl_ip.Size = new System.Drawing.Size(20, 13);
-            this.lbl_ip.TabIndex = 0;
-            this.lbl_ip.Text = "IP:";
             // 
             // gb_datosRecibidos
             // 
@@ -122,25 +77,10 @@
             this.gb_datosRecibidos.Controls.Add(this.txtb_datosRecibidos);
             this.gb_datosRecibidos.Location = new System.Drawing.Point(12, 64);
             this.gb_datosRecibidos.Name = "gb_datosRecibidos";
-            this.gb_datosRecibidos.Size = new System.Drawing.Size(515, 363);
+            this.gb_datosRecibidos.Size = new System.Drawing.Size(281, 363);
             this.gb_datosRecibidos.TabIndex = 3;
             this.gb_datosRecibidos.TabStop = false;
             this.gb_datosRecibidos.Text = "Datos Recibidos";
-            // 
-            // btn_respuesta
-            // 
-            this.btn_respuesta.Location = new System.Drawing.Point(430, 15);
-            this.btn_respuesta.Name = "btn_respuesta";
-            this.btn_respuesta.Size = new System.Drawing.Size(75, 23);
-            this.btn_respuesta.TabIndex = 6;
-            this.btn_respuesta.Text = "Respuesta";
-            this.btn_respuesta.UseVisualStyleBackColor = true;
-            this.btn_respuesta.Click += new System.EventHandler(this.btn_respuesta_Click);
-            // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // txtb_datosRecibidos
             // 
@@ -150,20 +90,25 @@
             this.txtb_datosRecibidos.Location = new System.Drawing.Point(10, 19);
             this.txtb_datosRecibidos.Name = "txtb_datosRecibidos";
             this.txtb_datosRecibidos.ReadOnly = true;
-            this.txtb_datosRecibidos.Size = new System.Drawing.Size(495, 338);
+            this.txtb_datosRecibidos.Size = new System.Drawing.Size(253, 338);
             this.txtb_datosRecibidos.TabIndex = 0;
             this.txtb_datosRecibidos.Text = "";
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
             // frm_server
             // 
-            this.AcceptButton = this.btn_conectar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(539, 439);
+            this.ClientSize = new System.Drawing.Size(305, 439);
             this.Controls.Add(this.gb_datosRecibidos);
             this.Controls.Add(this.grbx_datos);
             this.Name = "frm_server";
             this.Text = "Servidor";
+            this.Load += new System.EventHandler(this.frm_server_Load);
             this.grbx_datos.ResumeLayout(false);
             this.grbx_datos.PerformLayout();
             this.gb_datosRecibidos.ResumeLayout(false);
@@ -176,12 +121,7 @@
         private System.Windows.Forms.GroupBox grbx_datos;
         private System.Windows.Forms.TextBox txtb_port;
         private System.Windows.Forms.Label lbl_puerto;
-        private System.Windows.Forms.TextBox txtb_ip;
-        private System.Windows.Forms.Label lbl_ip;
-        private System.Windows.Forms.Button btn_conectar;
         private System.Windows.Forms.GroupBox gb_datosRecibidos;
-        private System.Windows.Forms.Button btn_desconectar;
-        private System.Windows.Forms.Button btn_respuesta;
         private System.Windows.Forms.RichTextBox txtb_datosRecibidos;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
--- TCP_Server/TCP_Server/Form1.cs
+++ TCP_Server/TCP_Server/Form1.cs
@@ -9,92 +9,84 @@ namespace TCP_Server
 {
     public partial class frm_server : Form
     {
-        public frm_server()
-        {
-            InitializeComponent();
-        }
-
-        string dataReceived = null;
         bool sortir = true;
-
         TcpClient client;
         NetworkStream nwStreamServer;
         TcpListener listener;
         Thread th_Escuchar;
 
-        private void btn_conectar_Click(object sender, EventArgs e)
+        public frm_server()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_server_Load(object sender, EventArgs e)
         {
             CheckForIllegalCrossThreadCalls = false;
-            th_Escuchar = new Thread(() => { dataReceived = EscucharCliente(); });
+            th_Escuchar = new Thread(EscucharCliente);
             th_Escuchar.Start();
+
+            this.FormClosed += (se, ev) => { sortir = false; };
         }
 
-        private void btn_desconectar_Click(object sender, EventArgs e)
+        private void EscucharCliente()
         {
-            try
-            {
-                client.Close();
-                listener.Stop();
-                nwStreamServer.Close();
-            }
-            catch
-            {
-
-            }
-        }
-
-        private void btn_respuesta_Click(object sender, EventArgs e)
-        {
-            RespuestaCliente("Respuesta del servidor: " + GetIP());
-        }
-
-        private string EscucharCliente()
-        {
-            string _dataRecived = "";
             try
             {
                 listener = new TcpListener(IPAddress.Any, Int32.Parse(txtb_port.Text.Trim()));
                 listener.Start();
                 while (sortir)
                 {
+                    //ESPERA UN MENSAJE DE ENTRADA
                     if (listener.Pending())
                     {
+                        //TRATAR MENSAJE DE ENTRADA
                         client = listener.AcceptTcpClient();
-
-                        nwStreamServer = client.GetStream();
                         byte[] buffer = new byte[client.ReceiveBufferSize];
+                        nwStreamServer = client.GetStream();
+                        int bytesLength= nwStreamServer.Read(buffer, 0, client.ReceiveBufferSize);
+                        string _dataRecived = Encoding.ASCII.GetString(buffer);
 
-                        int bytesRead = nwStreamServer.Read(buffer, 0, client.ReceiveBufferSize);
-
-                        _dataRecived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                        txtb_datosRecibidos.Text = _dataRecived;
-                        //sortir = false;
-                    }
+                        txtb_datosRecibidos.Text += "\n"+_dataRecived;
+                        
+                        //DEVOLVER UNA RESPUESTA
+                        RespuestaCliente("Respuesta recibida");
+                    } 
                 }
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            return _dataRecived;
         }
 
 
-        private bool RespuestaCliente(string respuesta)
+        private void RespuestaCliente(string respuesta)
         {
             try
             {
                 byte[] nouBuffer = Encoding.ASCII.GetBytes(respuesta);
                 nwStreamServer = client.GetStream();
                 nwStreamServer.Write(nouBuffer, 0, nouBuffer.Length);
-                return true;
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return false;
             }
         }
+        #region FUNCIONES INUTILES
+        //PARA DESCONECTARSE --APARENTEMENTE INUTIL
+        //private void btn_desconectar_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        client.Close();
+        //        listener.Stop();
+        //        nwStreamServer.Close();
+        //    }
+        //    catch
+        //    {}
+        //}
 
         private string GetIP()
         {
@@ -103,5 +95,6 @@ namespace TCP_Server
             IPAddress[] addr = ipEntry.AddressList;
             return addr[addr.Length - 1].ToString();
         }
+        #endregion
     }
 }
