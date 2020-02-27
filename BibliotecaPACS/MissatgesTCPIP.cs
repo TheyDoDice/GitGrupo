cb47711c8a1fb2e7750a25b69c8e73296ac3baef
jordi2505 <55592657+jordi2505@users.noreply.github.com>
2020-02-27 18:26:08 +0100

    Update MissatgesTCPIP.cs

--- BibliotecaPACS/MissatgesTCPIP.cs
+++ BibliotecaPACS/MissatgesTCPIP.cs
@@ -1,18 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BibliotecaPACS
 {
     class MissatgesTCPIP
     {
-        public string CrearMissatgeEntryRequirement (string Data,string IdentificadorNau, string IdentificadorEntrega)
+        public string CrearMissatgeEntryRequirement (string IdentificadorNau, string IdentificadorEntrega)
         {
-            string Fecha = DateTime.ParseExact(Data, "dd'/'MM'/'yyyy", CultureInfo.InvariantCulture).ToString("yyyy'/'MM'/'dd")
+            string MensajeER;
+            string Fecha, str_IdentificadorNau, str_IdentificadorEntrega;
+
+            Fecha = DateTime.Now.ToString("MMddyyyy");
+            str_IdentificadorNau = IdentificadorNau.Trim();
+            str_IdentificadorEntrega = IdentificadorEntrega.Trim();
+
+            if (str_IdentificadorNau.Length < 12)
+            {
+                str_IdentificadorNau = str_IdentificadorNau.PadLeft(12, '0');
+            }
+
+            if (str_IdentificadorEntrega.Length < 12)
+            {
+                str_IdentificadorEntrega = str_IdentificadorEntrega.PadLeft(12, '0');
+            }
+
+            MensajeER = "ER" + Fecha + str_IdentificadorNau + str_IdentificadorEntrega;
+
+            return MensajeER; 
         }
 
+        public string CrearMissatgeValidationResult(string IdentificadorNau, string Resultat)
+        {
+            string MensajeVR;
+            string str_IdentificadorNau;
+
+            str_IdentificadorNau = IdentificadorNau.Trim();
+
+            if (str_IdentificadorNau.Length < 12)
+            {
+                str_IdentificadorNau = str_IdentificadorNau.PadLeft(12, '0');
+            }
+
+            MensajeVR = "VR" + str_IdentificadorNau + Resultat;
+
+            return MensajeVR;
+        }
+
+        public string CrearMissatgeValidationKey (string CodiValidacioEncriptat)
+        {
+            string MensajeVK;
+
+            MensajeVK = "VK" + CodiValidacioEncriptat.Trim();
+
+            return MensajeVK;
+        }
     }
 }
