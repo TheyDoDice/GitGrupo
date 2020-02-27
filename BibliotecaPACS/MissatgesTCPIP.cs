381c2c78ea119fcaee8d5ccd0f04074c458e893d
jordi2505 <55592657+jordi2505@users.noreply.github.com>
2020-02-27 17:48:05 +0100

    MensajesTCP

---
+++ BibliotecaPACS/MissatgesTCPIP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaPACS
+{
+    class MissatgesTCPIP
+    {
+        public string CrearMissatgeEntryRequirement (string Data,string IdentificadorNau, string IdentificadorEntrega)
+        {
+            string Fecha = DateTime.ParseExact(Data, "dd'/'MM'/'yyyy", CultureInfo.InvariantCulture).ToString("yyyy'/'MM'/'dd")
+        }
+
+    }
+}
